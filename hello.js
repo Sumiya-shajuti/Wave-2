29d493ea62cd73f872de9ecf6614e8b7b82fbff3
Sizan-Aminun <71691929+sizanaminun@users.noreply.github.com>
2021-09-07 14:11:17 +0600

    add

---
+++ hello.js
@@ -0,0 +1,26 @@
+// javascript first module.
+var time = 32;
+console.log(time);
+var money = 43;
+console.log(money);
+var work = 12;
+console.log(work);
+var peace = 100;
+console.log(peace);
+var date = 12;
+console.log(date);
+var name = 'iraBoti'
+
+// Module12.5
+// USE Of toUpperCase and toLowerCase
+var promise = "Hello I want to Be good developer"
+console.log(promise.toUpperCase());
+
+// define Position of any word or letter in string where counting starts from 0 not 1
+var story = "Hey onu,what's going on?Are you ok?"
+console.log(story.indexOf("You"));
+console.log(story.indexOf("you"));
+
+// Split(divide any word in a string )
+var story = "Hey onu,what's going on?Are you ok?"
+console.log(story.split("what"));
